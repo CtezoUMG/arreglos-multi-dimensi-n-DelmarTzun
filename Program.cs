7103c4b52afc2afce962c5709f2ce74e72cbdcbe
Delmar Danilo Tzun Perez <dtzunp@miumg.edu.gt>
2026-02-24 23:21:05 -0600

    Implementación de búsqueda

--- Program.cs
+++ Program.cs
@@ -15,11 +15,17 @@ for (int i = 0; i < n; i++)
 }
 
 // --- INICIO DE LÓGICA DEL ALUMNO ---
-// Instrucción: El alumno debe encontrar el valor máximo del arreglo 'numeros'
-int maximo = numeros[0]; 
 
+// Instrucción: El alumno debe encontrar el valor máximo del arreglo 'numeros'
+  int maximo = numeros[0];
 // TODO: Implementar el algoritmo de búsqueda aquí
-for (int i = 1; i < numeros.Length; i++) { if (numeros[i] > maximo)    maximo = numeros[i];}
+ 
+        foreach (int num in numeros)
+        {
+            if (num > maximo)
+                maximo = num;
+        }
+       
 // --- FIN DE LÓGICA DEL ALUMNO ---
 
 // 2. Salida: Lo que el Autograding comparará
